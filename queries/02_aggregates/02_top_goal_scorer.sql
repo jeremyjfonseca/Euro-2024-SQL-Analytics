6011b4f04804e4e667e62790d52790da5bbf5292
Jeremy Fonseca <jeremy.fonseca@outlook.com>
2026-03-06 17:22:20 -0500

    Updated the aggregate queries to be only showcasing aggregation rather than aggregation and joins (for separation purposes).

--- queries/02_aggregates/02_top_goal_scorer.sql
+++ queries/02_aggregates/02_top_goal_scorer.sql
@@ -1,21 +1,17 @@
 --Find the player with the highest number of goals.
--- first query will only return the first player alphabetically from the list of top scorers (if there are multiple players with the same amount of goals at the top)
-SELECT p.name, COUNT(*) AS goal_count
-FROM project_player AS p
-INNER JOIN project_match_event AS m
-    ON p.player_id = m.player_id
+-- first query will only return the first player in id number order from the list of top scorers (if there are multiple players with the same amount of goals at the top)
+SELECT m.player_id, COUNT(*) AS goal_count
+FROM project_match_event AS m
 WHERE m.shot_outcome = 'Goal'
-GROUP BY p.player_id, p.name
+GROUP BY m.player_id
 ORDER BY goal_count DESC
 LIMIT 1;
 
 -- second query that will return all players that have the most amount of goals scored (all the top scorers in the tournament if tied)
-SELECT p.name, COUNT(*) AS goal_count
-FROM project_player AS p
-INNER JOIN project_match_event AS m
-    ON p.player_id = m.player_id
+SELECT m.player_id, COUNT(*) AS goal_count
+FROM project_match_event AS m
 WHERE m.shot_outcome = 'Goal'
-GROUP BY p.player_id, p.name
+GROUP BY m.player_id
 HAVING COUNT(*) = (
     SELECT MAX(goal_total)
     FROM (
--- queries/02_aggregates/03_team_highest_total_xg.sql
+++ queries/02_aggregates/03_team_highest_total_xg.sql
@@ -1,10 +1,8 @@
 --Find the team with highest cumulative expected goals
 
-select t.name, SUM(shot_statsbomb_xg) as cumulative_xg
-from project_team as t
-inner join project_match_event as m
-        on t.team_id = m.team_id
-where m.shot_statsbomb_xg is NOT NULL
-group by t.team_id, t.name
+select m.team_id, SUM(m.shot_statsbomb_xg) as cumulative_xg
+from project_match_event as m
+where m.shot_statsbomb_xg is NOT NULL and minute < 120
+group by m.team_id
 order by cumulative_xg desc
 limit 1
